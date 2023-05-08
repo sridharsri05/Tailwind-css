328993826c0b7301148492673ef2f97f97298021
Sridhar <srissri441@gmail.com>
2023-05-08 17:18:44 +0530

    tailwind practices

---
+++ tailwind-demo/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar"
+import Filters from "./components/Filters";
+import Items from "./components/Items";
+import SeasonSpecials from "./components/SeasonSpecials";
+import Footer from './components/Footer';
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Filters />
+      <Items />
+      <SeasonSpecials />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+
+
